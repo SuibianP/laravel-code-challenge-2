a12e497dd095a696bf4cd29d3c8579aa40e91559
Hu Jialun <jialun@aspireapp.com>
2021-12-07 22:58:33 +0800

    Amend repayment components

--- app/Models/ScheduledRepayment.php
+++ app/Models/ScheduledRepayment.php
@@ -5,10 +5,45 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Notifications\Notifiable;
 
 class ScheduledRepayment extends Model
 {
-    use HasFactory;
+    use HasFactory, Notifiable;
+
+    public function repaid() {
+        $this->outstanding_amount = 0;
+        $this->status = ScheduledRepayment::STATUS_REPAID;
+    }
+
+    public function updateRepaid() {
+        if ($this->outstanding_amount == 0 || $this->status == ScheduledRepayment::STATUS_REPAID) {
+            $this->repaid();
+        }
+    }
+
+    protected static function booted()
+    {
+        static::creating(function (ScheduledRepayment $scheduledRepayment) {
+            $scheduledRepayment->outstanding_amount =
+                $scheduledRepayment->outstanding_amount ?? $scheduledRepayment->amount;
+            $scheduledRepayment->updateRepaid();
+            if ($scheduledRepayment->status != ScheduledRepayment::STATUS_DUE) {
+                $scheduledRepayment->load('loan');
+                $scheduledRepayment->loan->refresh();
+                $scheduledRepayment->loan->outstanding_amount -=
+                    $scheduledRepayment->amount - $scheduledRepayment->outstanding_amount;
+                $scheduledRepayment->loan->save();
+            }
+        });
+        static::updating(function (ScheduledRepayment $scheduledRepayment) {
+            $scheduledRepayment->updateRepaid();
+            if ($scheduledRepayment->outstanding_amount < $scheduledRepayment->amount &&
+                $scheduledRepayment->outstanding_amount > 0) {
+                $scheduledRepayment->status = ScheduledRepayment::STATUS_PARTIAL;
+            }
+        });
+    }
 
     public const STATUS_DUE = 'due';
     public const STATUS_PARTIAL = 'partial';
@@ -28,6 +63,12 @@ class ScheduledRepayment extends Model
      */
     protected $fillable = [
         //
+        'outstanding_amount',
+        'amount',
+        'loan_id',
+        'currency_code',
+        'due_date',
+        'status',
     ];
 
     /**
@@ -35,7 +76,7 @@ class ScheduledRepayment extends Model
      *
      * @return BelongsTo
      */
-    public function loan()
+    public function loan(): BelongsTo
     {
         return $this->belongsTo(Loan::class, 'loan_id');
     }
